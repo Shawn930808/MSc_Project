0a5fee929457d8d4a967d149468c06c628529e55
unknown <wanghongshuo1993519@Outlook.com>
2021-09-10 21:36:08 +0800

    project

--- utils.py
+++ utils.py
@@ -3,17 +3,21 @@ import torch
 import shutil
 import numpy as np
 
+
 def save_net(fname, net):
     with h5py.File(fname, 'w') as h5f:
         for k, v in net.state_dict().items():
             h5f.create_dataset(k, data=v.cpu().numpy())
+
+
 def load_net(fname, net):
     with h5py.File(fname, 'r') as h5f:
-        for k, v in net.state_dict().items():        
-            param = torch.from_numpy(np.asarray(h5f[k]))         
+        for k, v in net.state_dict().items():
+            param = torch.from_numpy(np.asarray(h5f[k]))
             v.copy_(param)
-            
+
+
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')            
+        shutil.copyfile(filename, 'model_best.pth.tar')
